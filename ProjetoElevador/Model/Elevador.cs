43a8475bad8efc93dd5f1fc9f6d277d5e2894e6e
AlexandreSilva13 <95254688+AlexandreSilva13@users.noreply.github.com>
2022-01-06 09:17:59 -0300

    PrimeiroEnvio

---
+++ ProjetoElevador/Model/Elevador.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoElevador.Model
+
+{
+
+
+    public class Elevador
+    {
+        public Elevador() // Contrutor
+        {
+            AndarAtual = 0;
+            QtdPessoas = 0;
+        }
+
+        #region criando as propriedades
+        public int AndarAtual { get; set; }
+        public int TotalAndares { get; set; }
+        public int QtdPessoas { get; set; }
+        public int CapacidadeElevador { get; set; }
+        #endregion
+
+        #region Criando os Metodos
+        public void Inicializar(int capacidadeElevador, int totalAndares)
+        {
+            CapacidadeElevador = capacidadeElevador;
+            TotalAndares = totalAndares;
+        }
+
+        public string Entrar()
+        {
+            if (QtdPessoas <= CapacidadeElevador)
+            {
+                QtdPessoas++;
+                if (AndarAtual == 0)
+                    return "Entrou uma pessoa, elevador no térreo e tem " + QtdPessoas + " pessoa(s) no elevador";
+                else
+                    return "Entrou uma pessoa, elevador no " + AndarAtual + "º andar e tem " + QtdPessoas + " pessoa(s) no elevador";
+            }
+            else
+            {
+                return "Elevador cheio,favor aguardar o próximo";
+
+            }
+        }
+
+        public string Sair()
+        {
+            if (QtdPessoas > 0)
+            {
+
+                QtdPessoas--;
+                return "Saiu uma pessoa, elevador no " + AndarAtual + "º andar e tem " + QtdPessoas + " pessoa(s) no elevador";
+            }
+
+            else
+            {
+                return "Elevador vazio";
+
+            }
+        }
+
+        public string Subir()
+        {
+            if (AndarAtual <= TotalAndares)
+            {
+                AndarAtual++;
+                return "Subiu um andar, atualmente ele está no " + AndarAtual + "º andar e tem " + QtdPessoas + " pessoa(s) no elevador";
+            }
+
+            else
+            {
+                return "Elevador não pode subir,já está no ultimo andar e tem ocupação de " + QtdPessoas + " pessoa(s) no elevador";
+
+            }
+        }
+
+        public string Descer()
+        {
+            if (AndarAtual > 0)
+            {
+                AndarAtual--;
+                return "Desceu um andar, atualmente ele está no " + AndarAtual + "º andar e tem " + QtdPessoas + " pessoa(s) no elevador";
+
+            }
+            else
+            {
+                return "Elevador não pode descer, já está no térreo";
+
+            }
+        }
+        #endregion
+    }
+
+
+}
+
+
---
+++ ProjetoElevador/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using ProjetoElevador.Model;
+
+namespace ProjetoElevador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Elevador elevador = new Elevador();
+
+            Console.WriteLine("Seja bem vindo ao Elevador Jornada Microsolft");
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("Digite a quantidade de andares");
+            string totalAndares = Console.ReadLine();
+            Console.WriteLine("-------------------------------------------------");
+
+            Console.WriteLine("Digite a capacidade do elevador");
+            string capacidadeElevador = Console.ReadLine();
+            Console.WriteLine("-------------------------------------------------");
+
+            #region Laço de repetição
+
+            int numero = 0;
+            do
+
+            {
+                Console.WriteLine("1 Digite para Inicializar");
+                Console.WriteLine("2 Digite para entrar");
+                Console.WriteLine("3 digite para sair");
+                Console.WriteLine("4 digite para subir");
+                Console.WriteLine("5 digite para Descer");
+                Console.WriteLine("6 digite para finalizar");
+                string textoDigitado = Console.ReadLine();
+                numero = Convert.ToInt32(textoDigitado);
+
+                elevador.Inicializar(Convert.ToInt32(capacidadeElevador), Convert.ToInt32(totalAndares));
+                Console.WriteLine("Elevador Inicializado");
+                Console.WriteLine("-------------------------------------------------");
+                string retorno;
+                
+                switch (numero)
+                { 
+                    case 1:
+                        elevador.Inicializar(Convert.ToInt32(capacidadeElevador), Convert.ToInt32(totalAndares));
+                        break;
+
+                    case 2:
+                        retorno = elevador.Entrar();
+                        Console.WriteLine(retorno);
+                        break;
+
+                    case 3:
+                        retorno = elevador.Sair();
+                        Console.WriteLine(retorno);
+                        break;
+
+                    case 4:
+                        retorno = elevador.Subir();
+                        Console.WriteLine(retorno);
+                        break;
+
+                    case 5:
+                        retorno = elevador.Descer();
+                        Console.WriteLine(retorno);
+                        break;
+
+                    case 6:
+                        Console.WriteLine("Fim do programa");
+                        break;
+
+                    default:
+                        Console.WriteLine("Numero incorreto");
+                        break;
+                }
+
+            } while (numero != 6);   
+            #endregion
+    
+        }
+    }
+}
